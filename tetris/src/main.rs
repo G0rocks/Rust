818a88c4ad0a29c8579dc238ad09caf19c0d406d
Huldar <44577486+G0rocks@users.noreply.github.com>
2020-07-24 16:02:06 +0000

    Create main.rs

---
+++ tetris/src/main.rs
@@ -0,0 +1,20 @@
+mod functions; //declare functions.rs as a module
+
+//Define each tetrimino shape
+
+//Make a method for moving them down 1 step
+
+//Make a method for rotating them clockwise and counterclockwise
+
+fn main() {
+    println!("Hello, world!");
+        
+    //Game loop
+    while functions::game_is_on() {
+
+        //play music - Tetris ogg file?
+
+        //Check if the game is over
+    }
+}
+
