6d094eb489ee37f44e2d495fabda7c6b2236e3c9
Huldar <huldarh@gmail.com>
2021-12-12 23:10:00 +0000

    Relocate removed function to more appropriate location

--- tetris/src/functions.rs
+++ tetris/src/functions.rs
@@ -5,17 +5,14 @@
 */
 
 //Function which returns false if the game is over and true if the game is still on
-
 pub fn game_is_on(top_line:u8, tetris_zone_height:u8) -> bool {
+  println!("game_is_on_top function running");
+  println!("{}", top_line);
+
   //check if we have topped out
   if top_line < tetris_zone_height {
     return true;
   }
-  else {
+
   return false;
-  }
-}
-
-pub fn mute_audio() {
-
 }
