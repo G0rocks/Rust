5ec77abf4f3f5987c46a3d7e1c9f94ea6055a6bd
Huldar <huldarh@gmail.com>
2021-12-12 19:03:37 +0000

    Create constants, to make colors easier to access

---
+++ tetris/src/constants.rs
@@ -0,0 +1,14 @@
+/*
+This file contains global constants such as colors
+*/
+
+// Colours
+const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+const YELLOW: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const GREEN: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const BLUE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const PURPLE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const ORANGE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const LIGHTBLUE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
