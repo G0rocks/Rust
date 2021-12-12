6688b7c44548de92318541f4bc5317237492aef0
Huldar <huldarh@gmail.com>
2021-12-12 23:09:41 +0000

    Create constants module for all constants

--- tetris/src/constants.rs
+++ tetris/src/constants.rs
@@ -3,12 +3,31 @@ This file contains global constants such as colors
 */
 
 // Colours
-const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-const YELLOW: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const GREEN: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const BLUE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const PURPLE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const ORANGE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const LIGHTBLUE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+pub const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+pub const YELLOW: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const GREEN: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const BLUE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const PURPLE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const ORANGE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const LIGHTBLUE: [f32; 4] = [1.0, 0.0, 0.0, 1.0];  
+
+
+// Stigagjöf (e. point system)
+/*
+Fyrir 1 línu - 100 stig
+Fyrir 2 línur - 250 stig
+Fyrir 3 línur - 400 stig
+Fyrir 4 línur (einnig þekkt sem Tetris) - 800 stig
+Fyrir að fá Tetris tvisvar í röð - 1200 stig (fást fyrir seinna Tetris-ið)
+Þegar leikmaður leggur tetrimino niður með því að hard drop-pa
+10 stig fyrir hvern reit sem tetrimino-inn féll niður um í hard dropp-inu
+*/
+pub const POINTS_1_LINE:u8 = 100;
+pub const POINTS_2_LINES:u8 = 250;
+pub const POINTS_3_LINES:u16 = 400;
+pub const POINTS_TETRIS:u16 = 800;
+pub const POINTS_DOUBLE_TETRIS:u16 = 1200;
+pub const POINTS_SQUARE_HARD_DROP:u8 = 10;
+
