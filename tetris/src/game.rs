29b223883925cf1067b135924d8c3a4bfd327ee1
Huldar <huldarh@gmail.com>
2021-12-17 11:19:39 +0000

    start implementing the update function

--- tetris/src/game.rs
+++ tetris/src/game.rs
@@ -11,7 +11,6 @@ use crate::constants;       // For the colours
 // Dependencies
 use opengl_graphics::GlGraphics;    //{GlGraphics, OpenGL};
 
-
 /////////////////////////////////////////////////////////////////////////
 
 pub struct Game {
@@ -35,10 +34,12 @@ impl Game {
     });
 
     // Render outlines of tetrion
-    let inner_square = graphics::rectangle::rectangle_by_corners(constants::WIN_SIZE_X*0.05, constants::WIN_SIZE_Y*0.05, self.zone_width as f64, self.zone_height as f64);
-    let outer_square = graphics::rectangle::rectangle_by_corners(constants::WIN_SIZE_X*0.05 - constants::BOARDER_THICKNESS, constants::WIN_SIZE_Y*0.05 - constants::BOARDER_THICKNESS, (self.zone_width as f64) + constants::BOARDER_THICKNESS, (self.zone_height as f64) + constants::BOARDER_THICKNESS);
+    let tetrion_inner_square = graphics::rectangle::rectangle_by_corners(constants::WIN_SIZE_X*0.05, constants::WIN_SIZE_Y*0.05, self.zone_width as f64, self.zone_height as f64);
+    let tetrion_outer_square = graphics::rectangle::rectangle_by_corners(constants::WIN_SIZE_X*0.05 - constants::BORDER_THICKNESS, constants::WIN_SIZE_Y*0.05 - constants::BORDER_THICKNESS, (self.zone_width as f64) + constants::BORDER_THICKNESS, (self.zone_height as f64) + constants::BORDER_THICKNESS);
 
     // Render score, level and such
+    let score_inner_square = graphics::rectangle::rectangle_by_corners(constants::WIN_SIZE_X*0.6, constants::WIN_SIZE_Y*0.2, constants::WIN_SIZE_X*0.95, constants::WIN_SIZE_Y*0.7);
+    let score_outer_square = graphics::rectangle::rectangle_by_corners(constants::WIN_SIZE_X*0.6 - constants::BORDER_THICKNESS, constants::WIN_SIZE_Y*0.2 - constants::BORDER_THICKNESS, constants::WIN_SIZE_X*0.95 + constants::BORDER_THICKNESS, constants::WIN_SIZE_Y*0.7 + constants::BORDER_THICKNESS);
     //let score_text = "Score: ";
     //let score_render = graphics::text(constants::WHITE, constants::FONT_SIZE, &score_text, cache: &mut C, transform: math::Matrix2d, g: &mut G)
 
@@ -46,15 +47,23 @@ impl Game {
     self.gl.draw(arg.viewport(), |c,gl|{
       let transform = c.transform;
 
-      graphics::rectangle(constants::WHITE, outer_square, transform, gl);
-      graphics::rectangle(constants::BLACK, inner_square, transform, gl);
+      graphics::rectangle(constants::WHITE, tetrion_outer_square, transform, gl);
+      graphics::rectangle(constants::BLACK, tetrion_inner_square, transform, gl);
+      graphics::rectangle(constants::WHITE, score_outer_square, transform, gl);
+      graphics::rectangle(constants::BLACK, score_inner_square, transform, gl);
     });
+
+      // Teiknum leiðbeiningar fyrir neðan tetris zone-ið
+
+      // Sínum á hvaða borði (e. level) leikmaðurinn er
+
+      // Sínum fjölda lína sem leikmaðurinn hefur tekist að láta hverfa
 
   
     // Render each tetrimino (the first one is the one on hold and the second one is the next tetrimino and the third one is the active tetrimino)
-    //for i in 0..self.minos.len() {
-      //self.minos[i].render(); //arg);
-    //}
+    for i in 0..self.minos.len() {
+      self.minos[i].render(); //arg);
+    }
   }
   
   //This is supposed to be an end game message:
@@ -73,9 +82,32 @@ impl Game {
     });
   }
     */
-        
+
   pub fn update(&mut self) {
+    // Firstly if nowe need a new tetrimino, add a new one
+    if 1 > self.minos.len() {
+      self.minos.push(tetrimino::Mino::new())
+    }
     //Here we need to move all the tetriminos like they're supposed to move or 
+
+    // Látum tetrimino-ana detta, einn í einu, rólega niður
+
+    // Snúum tetrimino-inum þegar leikmaðurinn ýtir á upp örina
+    // Færum tetrimino-inn til hægri/vinstri þegar leikmaðurinn ýtir á hægri/vinstri örvatakkana
+
+    // Látum tetrimino-inn detta hratt niður á meðan leikmaðurinn heldur niðri niður örvatakkanum
+
+    // Setja þá alla leið niður ef leikmaður ýtir á bilslánna - Þekkt sem "Hard drop"
+
+    // Þegar tetriminoarnir mynda heila, óbrotna, lárétta línu yfir allan leikskjáinn þá hverfa þeira og leikmaðurinn fær
+    // stig í samræmi við hversu margar línur hurfu samtímis.
+    // Láta alla kubba fyrir ofan línuna detta niður þegar línan/línurnar hverfa
+    // Færum leikmann á næsta borð (e. level) þegar leikmaður nær ákveðið mörgum stigum. Hraðinn eykst eftir því sem þú kemst á hærra borð
+
+    // Láta tetrimino-a stoppa þegar þeir rekast á annan tetrimino fyrir neðan sig
+    // Setja næsta tetrimino af stað þegar núverandi tetrimino stoppar
+    // Láta leikinn enda þegar tetrimino er stoppar og er fyrir ofan toppinn á skjánum
+    // Þegar leikurinn endar --> Sýna "GAME OVER" og stig leikmanns
   }
 
   /*
